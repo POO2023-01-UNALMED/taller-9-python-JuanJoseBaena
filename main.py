08ca4b68978ec843ad6fc79284cc5afeb92e4f1a
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-06-05 10:30:01 -0500

    Commit 2

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from tkinter import Tk, Button, Entry
+from tkinter import Tk, Button, Entry, StringVar, Label
 
 # Configuración ventana principal
 root = Tk()
 root.title("Calculadora POO")
 root.resizable(0,0)
-root.geometry("400x270")
+root.geometry("400x250")
 
 # Configuración pantalla de salida 
 pantalla = Entry(root, width=40, bg="black", fg="white", borderwidth=0, font=("arial", 18, "bold"))
 pantalla.grid(row=0, column=0, columnspan=4, padx=1, pady=0)
+
+#label = Label (pantalla, width = 41, height = 2, bg = "black", fg = "white", borderwidth = 0, textvariable = entrada)
+#label.grid(row = 0, column = 0)
 
 # Configuración botones
 boton_1 = Button(root, text="1", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=1, column=0, padx=1, pady=1)
@@ -23,9 +26,27 @@ boton_9 = Button(root, text="9", width=9, height=3, bg="white", fg="red", border
 
 boton_igual = Button(root, text="=", width=20, height=3, bg="red", fg="white", borderwidth=0, cursor=" hand2").grid(row=4, column=0, columnspan=2, padx=1, pady=1)
 boton_punto = Button(root, text=".", width=9, height=3, bg="spring green", fg="black", cursor="hand2", borderwidth=0).grid(row=4, column=2, padx=1, pady=1)
-boton_mas = Button(root, text="+", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=1, column=3, padx=1, pady=1)
+boton_mas   = Button(root, text="+", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=1, column=3, padx=1, pady=1)
 boton_menos = Button(root, text="-", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=2, column=3, padx=1, pady=1)
 boton_multiplicacion = Button(root, text="*",  width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=3, column=3, padx=1, pady=1)
 boton_division = Button(root, text="/", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=4, column=3, padx=1, pady=1)
+
+def tecla(f):
+    global var 
+    var = var + str(f)
+    entrada.set(var)
+
+def result():
+    global var
+    try:
+        a = (eval(var))
+        entrada.set(j)
+    except:
+        entrada.set("")
+    var = ""
+
+var = ""
+entrada = StringVar()
+
 
 root.mainloop()
